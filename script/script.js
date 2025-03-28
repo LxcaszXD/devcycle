862a24e25725e0339a77a789d4954eb0d786c484
lhsenac <lhsenac45@gmail.com>
2025-03-28 11:52:25 -0300

    animação sobre feita + ajustes de espaçamento

--- script/script.js
+++ script/script.js
@@ -97,6 +97,39 @@ gsap.fromTo(
   }
 );
 
+document.addEventListener("DOMContentLoaded", function () {
+ 
+  'use strict';
+ 
+  gsap.registerPlugin(ScrollTrigger);
+ 
+  let workInfoItems = document.querySelectorAll('.work__photo-item');
+  workInfoItems.forEach(function (item, index) {
+      item.style.zIndex = workInfoItems.length - index;
+  });
+ 
+  gsap.set(".work__photo-item", {
+      clipPath: function () {
+          return "inset(0px 0px 0px 0px)"
+      }
+  });
+ 
+  const animation = gsap.to('.work__photo-item:not(:last-child)', {
+      clipPath: function () {
+          return "inset(0px 0px 100% 0px)"
+      },
+      stagger: .4,
+      ease: "none"
+  });
+ 
+  ScrollTrigger.create({
+      trigger: '.work',
+      start: 'top top',
+      end: 'bottom bottom',
+      animation: animation,
+      scrub: 1
+  });
+});
 
 gsap.registerPlugin(ScrollTrigger);
 
